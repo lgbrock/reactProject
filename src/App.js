fd91d882f8f3a06a3ef0f4d7eac6238b56e914b3
Logan Brock <logan.g.brock@gmail.com>
2021-03-31 18:04:57 -0500

    update link

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dashboard.heroku.com/apps"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Heroku dashboard
         </a>
       </header>
     </div>
